8fd42bac718668addb5dbf02b9c6f4fc7b20abf0
AndreiPuchko <86929038+AndreiPuchko@users.noreply.github.com>
2022-07-04 14:50:04 +0200

    Update install_linux.sh

--- assets/install_linux.sh
+++ assets/install_linux.sh
@@ -1,5 +1,1 @@
-python3 -m pip install --upgrade pip \
-  && python3 -m venv q2rad \
-  && source q2rad/bin/activate \
-  && pip install --upgrade q2rad \
-  && q2rad
+python3 -m pip install --upgrade pip && python3 -m venv q2rad && source q2rad/bin/activate && pip install --upgrade q2rad && q2rad
