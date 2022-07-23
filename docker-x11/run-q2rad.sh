1e95295cf4718c9604a5a1ef441e609e31cfa2ea
Andrei Puchko <andrei.puchko@gmx.de>
2022-07-23 10:37:02 +0200

    2

---
+++ docker-x11/run-q2rad.sh
@@ -0,0 +1,12 @@
+docker build -t q2rad .
+
+mkdir -p q2rad_storage/Desktop
+chmod -R 777 q2rad_storage
+
+docker run --rm -it \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $(pwd)/q2rad_storage:/home/q2rad \
+    -e DISPLAY=$DISPLAY \
+    -u q2rad \
+    q2rad \
+    python3 -m q2rad
