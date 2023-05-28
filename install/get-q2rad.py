0987b516ef094ee98edac0d51d6ad60f7c1a5dbc
Andrei Puchko <andrei.puchko@gmx.de>
2023-05-28 22:07:10 +0200

    gr 17

--- install/get-q2rad.py
+++ install/get-q2rad.py
@@ -141,7 +141,7 @@ if os.path.isfile(activator):
                 f"{shebang}\n"
                 f'cd "{os.path.abspath(".")}"\n'
                 f"{start_prefix} "
-                f"'{py3bin}'"
+                f'"{py3bin}"'
                 f" -c {code_string} "
             )
             open(f"../start-q2rad.{script_ext}", "w").write(start_script)
@@ -153,6 +153,9 @@ if os.path.isfile(activator):
             st = os.stat(f"../start-q2rad.{script_ext}")
             os.chmod(f"../start-q2rad.{script_ext}", st.st_mode | stat.S_IEXEC)
             code_string = code_string.replace('"', "")
-        os.execv(py3bin, [py3bin, "-c", code_string])
+        try:
+            os.execv(py3bin, [py3bin, "-c", code_string])
+        except Exception as e:
+            print(f"Failed to start: {e}", RED)
 else:
     print("Failed to create virtual enviroment.", RED)
